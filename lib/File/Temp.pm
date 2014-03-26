b3a6990e7567960eb0aa870337466c7068af6107
zdm <zdm@softvisio.net>
2014-03-27 01:43:00 +0200

    mistake in call to _get_pid was corrected

--- lib/File/Temp.pm
+++ lib/File/Temp.pm
@@ -1014,6 +1014,10 @@ sub _parse_args {
   return( \@template, \%args );
 }
 
+sub _get_pid {
+	return *threads::tid{CODE} ? qq{$$-} . threads->tid : $$;
+}
+
 =head1 OBJECT-ORIENTED INTERFACE
 
 This is the primary interface for interacting with
@@ -1135,7 +1139,7 @@ sub newdir {
   return bless { DIRNAME => $tempdir,
                  REALNAME => $real_dir,
                  CLEANUP => $cleanup,
-                 LAUNCHPID => _get_pid(),
+                 LAUNCHPID => File::Temp::_get_pid(),
                }, "File::Temp::Dir";
 }
 
@@ -2503,15 +2507,11 @@ sub unlink_on_destroy {
   return $self->{CLEANUP};
 }
 
-sub _get_pid {
-	return *threads::tid{CODE} ? qq{$$-} . threads->tid : $$;
-}
-
 sub DESTROY {
   my $self = shift;
   local($., $@, $!, $^E, $?);
   if ($self->unlink_on_destroy && 
-      _get_pid() eq $self->{LAUNCHPID} && !$File::Temp::KEEP_ALL) {
+      File::Temp::_get_pid() eq $self->{LAUNCHPID} && !$File::Temp::KEEP_ALL) {
     if (-d $self->{REALNAME}) {
       # Some versions of rmtree will abort if you attempt to remove
       # the directory you are sitting in. We protect that and turn it
